82e533aaf17d2e82a6aa88acd068901b29bd5bf4
Amara_xo <114367454+AMARAXO@users.noreply.github.com>
2024-05-28 18:56:33 -0500

    password works same as form validation... i won

--- app.js
+++ app.js
@@ -20,32 +20,56 @@ form.addEventListener('submit', (e) => {
   // Check first name
   if (fName === '') {
     firstName.classList.add('error');
+    document.querySelector('#firstErr').classList.add('Err')
   } else {
     firstName.classList.remove('error');
+    document.querySelector('#firstErr').classList.remove('Err')
   }
   // Check last name
 
   if (lName === '') {
     lastName.classList.add('error');
+    document.querySelector('#lastErr').classList.add('Err')
   } else {
     lastName.classList.remove('error');
+    document.querySelector('#lastErr').classList.remove('Err')
   }
+  
+
+  
   // Check email
 
   if (!validateEmail(emailVal) || emailVal === '') {
     email.classList.add('error');
+    document.querySelector('#emailErr').classList.add('Err')
   } else {
     email.classList.remove('error');
+    document.querySelector('#firstErr').classList.remove('Err')
   }
 
   // Check password
 
   if (passwordVal === '') {
     password.classList.add('error');
+    document.querySelector('#PWErr').classList.add('Err')
   } else {
     password.classList.remove('error');
+    document.querySelector('#PWErr').classList.remove('Err')
   }
 });
+let eyeIcon = document.getElementById("eyeicon")
+let passwords = document.getElementById("password")
+
+eyeIcon.addEventListener("click", function(){
+  if (passwords.type === "password"){
+    passwords.type = "text";
+    eyeIcon.src ="./images/open eye.png";
+
+  }else{
+    passwords.type = "password";
+    eyeIcon.src ="./images/closed eye.png"
+  }
+})
 
 //Validate email
 function validateEmail(email) {
@@ -53,3 +77,17 @@ function validateEmail(email) {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+ function toggleVisibility() {
+    const lastNameEr = getElementById("lastNameError");
+    if (lName == '') {
+      lastNameEr.style.visibility = "visible";
+    }else{
+      lastNameEr.style.visibility = "hidden";
+    }
+  }
+  button = getElementById("button");
+  button.addEventListener("click", toggleVisibility);
+
+
+  
